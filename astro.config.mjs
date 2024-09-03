de4869a02b3eb01c105e43c1c1cf80b5fe770cde
Ezem98 <56078650+Ezem98@users.noreply.github.com>
2024-09-02 23:21:03 -0300

    output server

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "hybrid",
+  output: "server",
   adapter: cloudflare()
 });
