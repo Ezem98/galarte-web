d2202fe2b7b2061e89847ed7c7074500a5576ded
Ezem98 <56078650+Ezem98@users.noreply.github.com>
2024-09-02 22:05:38 -0300

    fix again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  // adapter: vercel()
+  adapter: vercel()
 });
