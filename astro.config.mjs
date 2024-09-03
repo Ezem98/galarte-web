6a370e622a0b7a187f9a2ff129d760a9aa5d5c82
Ezem98 <56078650+Ezem98@users.noreply.github.com>
2024-09-02 22:43:48 -0300

    fix 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: "hybrid",
   adapter: vercel()
 });
