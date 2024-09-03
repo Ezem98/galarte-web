13d936f4f1bfb9343461f556b051c2ab3e88380d
Ezem98 <56078650+Ezem98@users.noreply.github.com>
2024-09-02 23:04:15 -0300

    add cloudfrare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "hybrid",
-  adapter: vercel()
+  adapter: cloudflare()
 });
