5a9c57d2f2860cbed6c7240d056e46d308cc4358
Ezem98 <56078650+Ezem98@users.noreply.github.com>
2024-09-08 21:59:16 -0300

    shop index page

---
+++ src/const/items.ts
@@ -0,0 +1,62 @@
+export const ITEMS = [
+  {
+    id: 1,
+    name: "Item 1",
+    image: "item1.jpg",
+    price: 100,
+  },
+  {
+    id: 2,
+    name: "Item 2",
+    image: "item2.jpg",
+    price: 200,
+  },
+  {
+    id: 3,
+    name: "Item 3",
+    image: "item3.jpg",
+    price: 300,
+  },
+  {
+    id: 4,
+    name: "Item 4",
+    image: "item4.jpg",
+    price: 400,
+  },
+  {
+    id: 5,
+    name: "Item 5",
+    image: "item5.jpg",
+    price: 500,
+  },
+  {
+    id: 6,
+    name: "Item 6",
+    image: "item6.jpg",
+    price: 600,
+  },
+  {
+    id: 7,
+    name: "Item 7",
+    image: "item7.jpg",
+    price: 700,
+  },
+  {
+    id: 8,
+    name: "Item 8",
+    image: "item8.jpg",
+    price: 800,
+  },
+  {
+    id: 9,
+    name: "Item 9",
+    image: "item9.jpg",
+    price: 900,
+  },
+  {
+    id: 10,
+    name: "Item 10",
+    image: "item10.jpg",
+    price: 1000,
+  },
+];
