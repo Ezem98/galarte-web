f056ed0d6cfbed55bccc25eb79162e056c1f9371
Ezem98 <56078650+Ezem98@users.noreply.github.com>
2024-10-11 13:35:26 -0300

    artists page + artist page

---
+++ src/const/artists.ts
@@ -0,0 +1,22 @@
+export const ARTISTS = [
+  {
+    id: "ornella-pocetti",
+    name: "Ornella Pocetti",
+    image: "ornella_pocetti.jpg",
+    biography: `Ornella Pocetti nace en Buenos Aires en 1991. Estudió Artes Visuales en la
+            Universidad Nacional de Arte de Buenos Aires, formándose en paralelo en
+            diversos talleres y programas.<br>
+            En el 2015 realiza su primera muestra individual en la galería Acéfala de
+            Buenos Aires y en 2019 recibe la beca del reconocido Programa Artistas x
+            Artistas. Asimismo forma parte del colectivo artístico "Viento Dorado" y
+            realiza ilustración editorial infantil.<br>
+            Sus obras han sido seleccionadas en el concurso Salón Nacional (2021),
+            Salón Felix Amador (2018 y 2019) en el Fondo Nacional de las Artes (2018),
+            Premio Itaú (2019), Salón Nacional de Rosario Castagnino - Macro (2018) y
+            en el X Premio Nacional de Pintura Banco Central (2017), recibiendo una
+            mención en los últimos cuatro.`,
+    age: 21,
+    nationality: "Argentina",
+    discipline: "Oleo sobre tela",
+  },
+];
