5de1961ad9f94319fe4bdaeba2cc64364625706b
Ezem98 <56078650+Ezem98@users.noreply.github.com>
2025-01-07 20:36:02 -0300

    upgrade astro + install react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import cloudflare from '@astrojs/cloudflare';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   output: "server",
   adapter: cloudflare()
 });
