d671d083a47fbd15897aa816884b54789111e832
Ezem98 <56078650+Ezem98@users.noreply.github.com>
2025-01-24 15:31:41 -0300

    improves

--- src/components/react/ArtistsSection.tsx
+++ src/components/react/ArtistsSection.tsx
@@ -1,3 +1,5 @@
+import ArtistImage from "@components/ArtistImage.astro";
+import { API_URL } from "@const/consts";
 import { useEffect, useState } from "react";
 import type { IArtist } from "src/types/artist";
 import type { ServiceResponse } from "src/types/serviceResponse";
@@ -6,16 +8,10 @@ export const ArtistsSectionComponent: React.FC = () => {
   const [artists, setArtists] = useState<IArtist[]>([]);
   const [artistId, setArtistId] = useState(0);
 
-  const apiURL = import.meta.env.PUBLIC_API_URL;
-
   const handleFetchArtists = async () => {
-    const response = await fetch(`${apiURL}/artists`);
+    const response = await fetch(`${API_URL}/artists`);
     const { data }: ServiceResponse<IArtist[]> = await response.json();
     setArtists(data);
-  };
-
-  const handleNavigateToArtist = (artist: IArtist) => {
-    location.href = `/artists/${artist.name.toLowerCase()}-${artist.lastname.toLowerCase()}-${artist.id}`;
   };
 
   useEffect(() => {
@@ -24,26 +20,26 @@ export const ArtistsSectionComponent: React.FC = () => {
 
   return (
     <section className="grid grid-rows-2 md:grid-rows-1 md:grid-cols-2 overflow-hidden">
-      <section className="flex flex-col items-center md:items-end gap-2 text-4xl md:text-6xl font-bold py-8 md:p-0 md:pt-20 pr-8 md:border-r md:row-start-1 md:row-end-2">
+      <section className="flex flex-col items-center md:items-end gap-2 font-bold py-8 md:p-0 md:pt-20 pr-8 md:border-r md:row-start-1 md:row-end-2">
         {artists.map((artist) => (
-          <h3
-            className="cursor-pointer hover:text-secondary transition-all duration-300 ease-in-out"
-            onMouseEnter={() => setArtistId(artist.id)}
-            onMouseLeave={() => setArtistId(0)}
-            onClick={() => handleNavigateToArtist(artist)}
-          >{`${artist.name} ${artist.lastname}`}</h3>
+          <a
+            href={`/artists/${artist.name.toLowerCase()}-${artist.lastname.toLowerCase()}-${artist.id}`}
+          >
+            <h3
+              className="cursor-pointer hover:text-secondary transition-all duration-300 ease-in-out text-4xl"
+              onMouseEnter={() => setArtistId(artist.id)}
+              onMouseLeave={() => setArtistId(0)}
+            >{`${artist.name} ${artist.lastname}`}</h3>
+          </a>
         ))}
       </section>
 
       {artistId !== 0 && (
-        <section className="justify-start overflow-hidden">
-          <img
-            id="artist-image"
-            src={artists.find((artist) => artist.id === artistId)?.image}
-            alt="Hovered Artist's Image"
-            className="w-full md:w-3/5 md:pt-20 md:pl-8 md:pb-8 aspect-square"
-          />
-        </section>
+        <ArtistImage
+          artistImage={
+            artists.find((artist) => artist.id === artistId)?.image ?? ""
+          }
+        />
       )}
     </section>
   );
