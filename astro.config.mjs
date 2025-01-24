d5808ac8e4e82d35fd10e97ad0a04dba9f26c10c
Ezem98 <56078650+Ezem98@users.noreply.github.com>
2025-01-24 15:44:23 -0300

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import cloudflare from '@astrojs/cloudflare';
-
-import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind()],
   output: "server",
   adapter: cloudflare()
 });
