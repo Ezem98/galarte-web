6fb360d3ba014b8cda0ed674bdb8e8e56454d38b
Ezem98 <56078650+Ezem98@users.noreply.github.com>
2025-02-21 17:06:40 -0300

    fix about us page

--- src/const/about-us.ts
+++ src/const/about-us.ts
@@ -1,4 +1,4 @@
-export const ABOUT_US = [
+export const ABOUT_US_MOBILE = [
   {
     id: 1,
     paragraph: `Galarte es una galería de arte contemporáneo online. Nuestro objetivo es
@@ -51,6 +51,47 @@ export const ABOUT_US = [
   {
     id: 8,
     paragraph: `Como equipo de la galería aspiramos a que
+      esto no sea sólo una galería; deseamos crecer junto a los artistas en un
+      camino que pueda decantar en muchísimas posibilidades. Esperamos que tus
+      deseos se alineen a los nuestros y poder, así, iniciar
+      juntos este proceso.`,
+  },
+];
+
+export const ABOUT_US = [
+  {
+    id: 1,
+    paragraph: `Galarte es una galería de arte contemporáneo online. Nuestro objetivo es
+      brindar apoyo integral a los artistas, guiándolos en la venta y
+      divulgación de sus obras. Buscamos ser el nexo entre las creaciones
+      artísticas y el consumidor de arte, facilitando que cada obra llegue a los
+      posibles y diversos públicos interesados. Buscamos piezas en diferentes
+      formatos y técnicas: desde pinturas, fotografías, grabados, ilustraciones
+      y esculturas hasta objetos útiles y accesorios atemporales. Valoramos el
+      buen diseño y la creatividad, y estamos abiertos a explorar nuevas
+      técnicas que enriquezcan nuestro catálogo. Creemos en el poder del arte
+      para transformar espacios y nos enfocamos en la venta de obras que los
+      embellecen. Seleccionamos piezas que transforman cualquier ambiente en un
+      lugar especial. Nos proponemos vincular el arte con lo doméstico y lo
+      cotidiano.`,
+  },
+  {
+    id: 2,
+    paragraph: `Galarte surge de la experiencia de una de nuestras socias,
+      quien comprendió la dificultad de equilibrar la creación artística con la
+      venta. Observamos que muchos artistas no tienen el tiempo ni las
+      habilidades necesarias para vender su arte, lo que puede convertirse en un
+      obstáculo significativo. Nuestro propósito es eliminar esta barrera,
+      permitiendo que los artistas se concentren en su trabajo creativo mientras
+      nosotros nos encargamos de la comercialización.`,
+  },
+  {
+    id: 3,
+    paragraph: `Nuestro compromiso desde
+      Galarte es tener una tienda online actualizada, realizar contenido de
+      redes y publicidad digital activa para la difusión y promoción de las
+      obras, la búsqueda constante de nuevos clientes y llevar a cabo la
+      logística de embalaje y envío. Como equipo de la galería aspiramos a que
       esto no sea sólo una galería; deseamos crecer junto a los artistas en un
       camino que pueda decantar en muchísimas posibilidades. Esperamos que tus
       deseos se alineen a los nuestros y poder, así, iniciar
